56539073919b58c32cefff42a2fa2e2f0446ba2f
mikekrom1 <52241579+mikekrom1@users.noreply.github.com>
2022-02-08 19:13:21 +0100

    Update blobhertel.sh

--- Supporting-files/blobhertel.sh
+++ Supporting-files/blobhertel.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-if [ "$1" -eq "This is a text string with secret code UI873632rt." ]; then 
+if [ $1 -eq "This is a text string with secret code UI873632rt." ]
+then 
   echo "succes" 
 else 
   exit 1
